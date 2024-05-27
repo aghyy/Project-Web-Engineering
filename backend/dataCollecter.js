f61efca48ed1c0e2af49c0f512797767980bc64c
fldoll <florian.doll@abilis.de>
2024-05-27 14:45:54 +0200

    parsing to xml

--- backend/dataCollecter.js
+++ backend/dataCollecter.js
@@ -1,43 +1,65 @@
 const axios = require("axios");
 const jsdom = require("jsdom");
+const js2xmlparser = require("js2xmlparser");
+const fs = require("fs");
 const headers = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 };
-const users = new Map();
-// 2023
-users.set('TINF23B1', 'freudenmann');
-users.set('TINF23B2', 'ritterbusch');
-users.set('TINF23B3', 'vollmer');
-users.set('TINF23B4', 'eisenbiegler');
-users.set('TINF23B5', 'strand');
-users.set('TINF23B6', 'li');
-
-// 2022
-users.set('TINF22B1', 'freudenmann');
-users.set('TINF22B2', 'ritterbusch');
-users.set('TINF22B3', 'vollmer');
-users.set('TINF22B4', 'eisenbiegler');
-users.set('TINF22B5', 'strand');
-users.set('TINF22B6', 'eisenbiegler');
-
-// 2021
-users.set('TINF21B1', 'freudenmann');
-users.set('TINF21B2', 'ritterbusch');
-users.set('TINF21B3', 'vollmer');
-users.set('TINF21B4', 'eisenbiegler');
-users.set('TINF21B5', 'strand');
-
+const users = JSON.parse(fs.readFileSync('../public/assets/json/courses.json', 'utf8'));
 async function performScraping(courseName, day, month, year) {
 	// HTTP GET request in Axios
-	const axiosResponse = await axios.request({
-		method: "GET",
-		url: `https://rapla.dhbw-karlsruhe.de/rapla?page=calendar&user=${users.get(courseName)}&file=${courseName}&day=${day}&month=${month}&year=${year}`,
-		headers: headers
+	return await axios.request({
+		method : "GET",
+		url : `https://rapla.dhbw-karlsruhe.de/rapla?page=calendar&user=${users[courseName]}&file=${courseName}&day=${day}&month=${month}&year=${year}`,
+		headers : headers
 	});
-	return axiosResponse;
 }
 
-const execute = async (courseName, day, month, year) => {
+function mapWeekDay(germanDay) {
+	const dayMapping = {
+		'Mo': 'mon',
+		'Di': 'tue',
+		'Mi': 'wed',
+		'Do': 'thu',
+		'Fr': 'fri'
+	};
+
+	return dayMapping[germanDay] || null;  // Return null if the input is not a valid key
+}
+
+parseToXml = (listOfLectureCurrentWeek) => {
+	const daysOfWeek = ["mon", "tue", "wed", "thu", "fri"];
+
+	let lessonsByDay = {}; // Object to accumulate lessons for each day
+
+	// Group lessons by day
+	daysOfWeek.forEach(day => {
+		lessonsByDay[day] = listOfLectureCurrentWeek.filter(obj => obj.week_day === day).map(lesson => ({
+			name: lesson.name,
+			person: lesson.person,
+			room: lesson.room,
+			total_time: lesson.total_time,
+			begin: lesson.begin,
+			end: lesson.end
+		}));
+	});
+
+	let daysXml = daysOfWeek.map(day => ({
+		"@": {
+			id: day
+		},
+		lesson: lessonsByDay[day]
+	}));
+
+	let xmlCalendar = {
+		course: listOfLectureCurrentWeek[0].course,
+		day: daysXml
+	};
+
+	return js2xmlparser.parse("calendar", xmlCalendar, {'declaration': {'encoding': 'UTF-8'}}); // Return the XML for the entire week without XML declaration
+}
+
+const getXml = async (courseName, day, month, year) => {
 	const htmlString = await performScraping(courseName, day, month, year);
 	let listOfLectureCurrentWeek = [];
 
@@ -46,7 +68,7 @@ const execute = async (courseName, day, month, year) => {
 		let wholeWeek = html.querySelectorAll('.week_block');
 
 		let course = html.querySelector('.week_block').querySelector('.resource').textContent;
-		listOfLectureCurrentWeek.push({"course": course});
+		listOfLectureCurrentWeek.push({ "course": course });
 
 		for (const element of wholeWeek) {
 			let begin = element.querySelector('.week_block a').textContent.slice(0, 5);
@@ -57,19 +79,24 @@ const execute = async (courseName, day, month, year) => {
 			let difDate = new Date(dif);
 			let timeDif = `${difDate.getHours() - 1}h ${difDate.getMinutes()}m`;
 
-			let weekDay = element.querySelectorAll('.tooltip div')[1].textContent.slice(0, 2);
+			let weekDay = mapWeekDay(element.querySelectorAll('.tooltip div')[1].textContent.slice(0, 2));
+
 			let jsonObject = {
-				name : element.querySelector('a').textContent.split(/Titel:\n|\n/)[3],
-				person : element.querySelector('.person')?.textContent ?? "",
-				room : element.querySelectorAll('.resource')[1].textContent,
-				total_time : timeDif,
-				begin : begin,
-				end : end,
+				name: element.querySelector('a').textContent.split(/Titel:\n|\n/)[3],
+				person: element.querySelector('.person')?.textContent ?? "",
+				room: element.querySelectorAll('.resource')[1].textContent,
+				total_time: timeDif,
+				begin: begin,
+				end: end,
+				week_day: weekDay
 			};
 			listOfLectureCurrentWeek.push(jsonObject);
 		}
-		console.log(listOfLectureCurrentWeek);
+
+		let parsedOutput = parseToXml(listOfLectureCurrentWeek);
+		console.log(parsedOutput);
+		// return parsedOutput;
 	}
 }
 
-execute("TINF23B6", 27, 5, 2024);
+getXml("TINF23B6", 13, 5, 2024);
